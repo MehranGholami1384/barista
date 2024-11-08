de37fbc9479bbc3bc40c184e4d3100fe2c163776
Mehran Gholami <mehrangholami666@outlook.com>
2024-11-08 15:04:10 +0330

    add section (login) to auth page

--- Js/app.js
+++ Js/app.js
@@ -90,6 +90,22 @@ $(document).ready(function () {
         $('.shop-head-title-section').removeClass('d-none')
     }
 
+    // auth page ---------------------------------------------------------------------------------------------------------
+
+    $('.toggle-show-password-btn').click(function (event) {
+        let showPasswordIcon = $(this).find($('.bi.bi-eye-fill'))[0]
+        event.preventDefault()
+
+        $(this).empty()
+        if (showPasswordIcon) {
+            $('#password-input').attr('type', 'text')
+            $(this).append(`<svg xmlns="http://www.w3.org/2000/svg" width="24" fill="currentColor" class="bi bi-eye-slash-fill color-gray10" viewBox="0 0 16 16"><path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/><path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/></svg>`)
+        } else {
+            $('#password-input').attr('type', 'password')
+            $(this).append(`<svg xmlns="http://www.w3.org/2000/svg" width="24" fill="currentColor" class="bi bi-eye-fill color-gray10" viewBox="0 0 16 16"><path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"></path><path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"></path></svg>`)
+        }
+    })
+
     // global ---------------------------------------------------------------------------------------------------------
 
     function formatPriceWithCommas() {
